55d9de051756c9d9a64e031b086257d45fba2853
jotapeqzz <joaopedrogoncalvesdelima@gmail.com>
2024-06-13 14:03:43 -0300

    aula de josi 2

---
+++ jp (2).py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print('adição', adicao)
+
+subtracao = 10 - 5
+print('subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('multiplicação',multiplicacao)
+
+divisao = 10 / 3 # float
+print('divisão',divisao)
+
+divisao_inteira =10 // 3
+print('divisão inteira',divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('módulo', modulo)
